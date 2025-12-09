182d837bd314425c2c179e29c073547182eac3b4
Xavier <xavier@windyplace.ch>
2025-12-09 17:12:36 +0100

    updated

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import sqlite3
 from urllib.parse import urljoin
 from tqdm.asyncio import tqdm_asyncio
 from aiohttp import ClientError, ClientConnectorError
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
+import numpy as np
 
 semaphore = asyncio.Semaphore(10)  # max 10 concurrent requests
 
@@ -49,9 +51,16 @@ refs = soup.find_all("a",string="Detail")
 
 links = [urljoin(BaseURL,link.get("href")) for link in refs]
 
-
-
 print("Got all the records URLs, we have ", len(links), "records")
+
+refoldDF = pd.read_table("REFOLD_161028.txt")
+
+refoldDF["pubmed_id"] = refoldDF["pubmed_id"].astype("Int64").astype("str")
+print("Loaded RefoldDF")
+
+# Function to get true columns in a row
+def true_columns(row):
+    return ', '.join(row.index[row.astype(bool)])
 
 async def FindMonashUrl(url, session):
     
@@ -155,19 +164,124 @@ async def gatherrecord(url, session):
         return data
 
 async def process_url(url, session):
+    row_record = None
     monash_url = await FindMonashUrl(url, session)
     if monash_url is not None:
         row_record = await gatherrecord(monash_url, session)
         row_record.append("Not needed")
         return row_record
+    elif monash_url is None:
+        row_record = await extract_from_db(url, session)
     pubmed_url = await FindPubMedURL(url, session)
     if pubmed_url is not None:
-        row = check_paper_row.copy()
+        if not row_record:
+            row_record = check_paper_row.copy()
         DOI = await FindDOI(pubmed_url, session)
         if DOI is not None:
-            row.append(DOI)
-            return row
+            row_record.append(DOI)
+            return row_record
     return no_data_row
+
+async def extract_from_db(url, session):
+    async with semaphore:
+        try:
+            async with session.get(url, timeout=aiohttp.ClientTimeout(total=10)) as response:
+                if response.status == 429:
+                    retry_after = int(response.headers.get("Retry-After", 5))
+                    print(f"Rate limited, sleeping {retry_after} seconds...")
+                    await asyncio.sleep(retry_after)
+                response.raise_for_status()  # raise error on bad status
+                html_content = await response.text()
+        except (aiohttp.ClientError, asyncio.TimeoutError) as e:
+            print(f"Error fetching {url}: {e}")
+            return None
+    
+        soup = BeautifulSoup(html_content, "html.parser")
+        
+
+        cell = soup.find("td", string="PubMed ID")
+        pubmed_id = cell.find_next_sibling("td").get_text(strip=True)
+
+
+        cell = soup.find("td", string = "Protein name")
+        protein_name = cell.find_next_sibling("td").get_text(strip=True)
+
+        cell = soup.find("td", string = "Comment")
+
+        try:
+            row = refoldDF[(refoldDF["pubmed_id"] == pubmed_id) & (refoldDF["name"] == protein_name)].iloc[0]
+        
+        except IndexError:
+            return None
+
+
+        refolding_method_columns =  ['dilution','dialysis','column_filtration','column_binding','high_pressure','other_method']
+
+        # Apply only to subset of columns
+        refolding_method = ', '.join(
+        row[refolding_method_columns].index[row[refolding_method_columns].astype(bool)]
+    )
+        
+
+        row_record = [row["name"], #Protein.Protein_Name
+                      "", # Protein.Abbreviated_Name
+                      "", # Protein.SCOP_Family
+                      "", # Protein.Structure_Notes
+                      "", # Protein.Organism
+                      row["uniprot_id"], # Protein.UniProt_Accession
+                      "", # Protein.SCOP_Unique_ID
+                      "", # Protein.Structure_Solved
+                      row["function"], # Protein.Class
+                      "",# Protein.Molecularity
+                      "", # Construct.Full_Length
+                      row["domain"], # Construct.Domain
+                      "", # Construct.Chimera
+                      "", # Construct.Variants
+                      len(row["aaseq"]) if pd.notna(row["aaseq"]) else "", # Construct.Chain_Length
+                      ProteinAnalysis(row["aaseq"]).molecular_weight() if pd.notna(row["aaseq"]) else "", # Construct.Molecular_Weight
+                      ProteinAnalysis(row["aaseq"]).isoelectric_point() if pd.notna(row["aaseq"]) else "", # Construct.Pi
+                                            ProteinAnalysis(row["aaseq"]).molecular_weight() if pd.notna(row["aaseq"]) else "", # Construct.Molecular_Weight DUPLICATE ON ORIGNAL DB
+                      "", # Construct.Disulphides
+                      row["aaseq"], # Construct.Full_Sequence
+                      row["comment"], # Construct.Notes
+                      row["author"]+ row["date"] + " " + row["journal"], # Expression.Report
+                      "", # Expression.Project_Aim
+                      "", # Expression.Fusion
+                      "", # Expression.Protein_Expression_and_Production
+                      "", # Expression.Expression_Host
+                      "", # Expression.Expression_Strain
+                      "", # Expression.Expression_Temp
+                      "", # Expression.Expression_Time
+                      "", # Expression.Expression_Vector
+                      "", # Expression.Expression_Protocol
+                      "", # Expression.Method_of_Induction
+                      "", # Expression.Cell_Density_at_Induction
+                      "", # Expression.Cell_Disruption_Method
+                      "", # Expression.Lytic_Agent
+                      "", # Expression.Pre-Refolding_Purification
+                      "", # Expression.Solubility
+                      refolding_method, # Refolding.Refolding_Method
+                      "", # Refolding.Wash_Buffer
+                      "", # Refolding.Solubilization_Buffer
+                      "", # Refolding.Refolding_Buffer
+                      "", # Refolding.Pre-Refolding_Purification
+                      "", # Refolding.Tag_Cleaved
+                      row["ph"], # Refolding.Refolding_pH
+                      row["temperature"], # Refolding.Refolding_Temperature
+                      "", # Refolding.Protein_Concentration
+                      "", # Refolding.Refolding_Time
+                      "", # Refolding.Redox_Agent
+                      "", # Refolding.Redox_Agent_Concentration
+                      "", # Refolding.Refolding_Protocol
+                      "", # Refolding.Refolding_Assay
+                      "", # Refolding.Refolding_Chaperones
+                      "", # Refolding.Refolding_Additives
+                      "", # Refolding.Additives_Concentration
+                      "", # Refolding.Refolding_Yield
+                      "", # Refolding.Purity
+                      "", # Refolding.Notes
+        ]
+        return row_record
 
 async def main():
 
@@ -181,8 +295,6 @@ async def main():
             links_values.append(result)
         return links_values
 
-
-
 if __name__ == "__main__":
     start = datetime.now()
     loop = asyncio.get_event_loop()
@@ -191,4 +303,4 @@ if __name__ == "__main__":
     df = df.loc[:, ~df.columns.duplicated()] # Deal with duplicate Molecular_weight name in the data
 
     conn = sqlite3.connect('REFOLD.db')
-    df.to_sql("FullDBTrial",con=conn, index=False)
+    df.to_sql("FullDBTrial5",con=conn, index=False)
